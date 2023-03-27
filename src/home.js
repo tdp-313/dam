569801948d75fb88d04fcf6f6792a53d5493fa58
tdp-313 <119167759+tdp-313@users.noreply.github.com>
2023-03-28 00:50:20 +0900

    linkmiss fix

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 async function home_html() {
     let home = "";
-    home += '<iframe id="iframe-monaco" title="Moncaca_from DAM" src="../monaca/"></iframe>';
+    home += '<iframe id="iframe-monaco" title="Moncaca_from DAM" src="./monaca/"></iframe>';
     return (home);
 }
