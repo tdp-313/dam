b5070a55aa952bcffb4977f71fc0f4dd91408fee
tdp-313 <119167759+tdp-313@users.noreply.github.com>
2023-04-05 23:50:21 +0900

    overwrite mode add

--- monaco/monaco.js
+++ monaco/monaco.js
@@ -26,7 +26,7 @@ const monacoStart = async () => {
     monaco.languages.setMonarchTokensProvider('rpg-indent', rpg_token2());
     monaco.editor.defineTheme('myTheme', theme_dark);
     //monaco.editor.telemetry = false;
-
+    const flag_regex = /\*IN[0-9][0-9]/;
     monaco.languages.registerDefinitionProvider('rpg-indent', {
       provideDefinition: function (model, position) {
         let row = model.getLineContent(position.lineNumber);
@@ -45,17 +45,36 @@ const monacoStart = async () => {
           let op_2 = row.substring(50, 60).trim();
           let fieldLen = row.substring(67, 70).trim();
           let result = row.substring(60, 66).trim();
-          if (op_m === 'PARM') {
-            if (wordStr === result) {
-              ranges.push({ range: new monaco.Range(i, 1, i, 70), uri: model.uri });
+          if (row.substring(6, 7) !== "*" && row.substring(5, 6) === "C") {
+            if (op_m === 'PARM') {
+              if (wordStr === result) {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              }
+            } else if (op_m === 'PLIST' || op_m === 'KLIST' || op_m === 'BEGSR') {
+              if (wordStr === op_1) {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              }
+            } else {
+              if (wordStr === result && fieldLen !== '') {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              } else if (wordStr === result && wordStr.substring(0, 1) === "*") {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              }
             }
-          } else if (op_m === 'PLIST' || op_m === 'KLIST' || op_m === 'BEGSR') {
-            if (wordStr === op_1) {
-              ranges.push({ range: new monaco.Range(i, 1, i, 70), uri: model.uri });
+            if (text.type === 'flag' || text.type === 'flag1' || text.type === 'flag2' || text.type === 'flag3') {
+              let wordStr_flag = "*IN" + wordStr;
+              if (wordStr_flag === result) {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              }
             }
-          } else {
-            if (wordStr === result && fieldLen !== '') {
-              ranges.push({ range: new monaco.Range(i, 1, i, 70), uri: model.uri });
+            if (flag_regex.test(wordStr)) {
+              let flag = [];
+              flag.push(row.substring(71, 73));
+              flag.push(row.substring(73, 75));
+              flag.push(row.substring(75, 77));
+              if (flag.includes(wordStr.substring(3, 5))) {
+                ranges.push({ range: new monaco.Range(i, 5, i, 77), uri: model.uri });
+              }
             }
           }
         }
@@ -75,29 +94,36 @@ const monacoStart = async () => {
         let lineCount = model.getLineCount();
         for (let i = 1; i < lineCount; i++) {
           let row = model.getLineContent(i);
-          let op_1 = row.substring(17, 27).trim();
-          //let op_m = row.substring(45, 50).trim();
-          let op_2 = row.substring(50, 60).trim();
-          //let fieldLen = row.substring(67, 70).trim();
-          let result = row.substring(60, 66).trim();
-          if (wordStr === op_1 || wordStr === op_2 || wordStr === result) {
-            ranges.push({ range: new monaco.Range(i, 1, i, 70), uri: model.uri });
-          }
-        }
-        if (text.type === 'flag' || text.type === 'flag1' || text.type === 'flag2' || text.type === 'flag3') {
-          let wordStr_flag = "*IN" + wordStr;
-          for (let i = 1; i < lineCount; i++) {
-            let row = model.getLineContent(i);
+          if (row.substring(6, 7) !== "*" && row.substring(5, 6) === "C") {
             let op_1 = row.substring(17, 27).trim();
             //let op_m = row.substring(45, 50).trim();
             let op_2 = row.substring(50, 60).trim();
             //let fieldLen = row.substring(67, 70).trim();
-            if (wordStr_flag === op_1 || wordStr_flag === op_2) {
-              ranges.push({ range: new monaco.Range(i, 1, i, 70), uri: model.uri });
+            let result = row.substring(60, 66).trim();
+
+            if (wordStr === op_1 || wordStr === op_2 || wordStr === result) {
+              ranges.push({ range: new monaco.Range(i, row.indexOf(wordStr) + 1, i, row.lastIndexOf(wordStr) + wordStr.length + 1), uri: model.uri });
+            }
+            if (text.type === 'flag' || text.type === 'flag1' || text.type === 'flag2' || text.type === 'flag3') {
+              let wordStr_flag = "*IN" + wordStr;
+              if (wordStr_flag === op_1 || wordStr_flag === op_2) {
+                ranges.push({ range: new monaco.Range(i, row.indexOf(wordStr_flag) + 1, i, row.lastIndexOf(wordStr_flag) + wordStr_flag.length + 1), uri: model.uri });
+              }
+            }
+            if (flag_regex.test(wordStr)) {
+              let flag = [];
+              flag.push(row.substring(9, 11));
+              flag.push(row.substring(12, 14));
+              flag.push(row.substring(15, 17));
+              flag.push(row.substring(71, 73));
+              flag.push(row.substring(73, 75));
+              flag.push(row.substring(75, 77));
+              if (flag.includes(wordStr.substring(3, 5))) {
+                ranges.push({ range: new monaco.Range(i, row.indexOf(wordStr.substring(3, 5)) + 1, i, row.lastIndexOf(wordStr.substring(3, 5)) + 3), uri: model.uri });
+              }
             }
           }
         }
-
         return ranges;
       }
     });
@@ -245,6 +271,20 @@ const monacoStart = async () => {
       diffEditor.layout();
       extraControlClick(true);
     });
+    let isInsert = true;
+    const insertChange = document.getElementById('control-extraInsertText');
+    insertChange.addEventListener('click', (e) => {
+      isInsert = e.target.checked;
+      insertIconUpdate();
+    });
+    const insertIconUpdate = () => {
+      if (isInsert) {
+        normalEditor.updateOptions({ cursorStyle: "line-thin" });
+      } else {
+        normalEditor.updateOptions({ cursorStyle: "block" });
+      }
+      insertChange.checked = isInsert;
+    }
     const rulerChange = (isDisp) => {
       if (isDisp) {
         normalEditor.updateOptions({ rulers: [5, 6, 17, 27, 45, 50, 60, 66, 69, 71, 77] });
@@ -296,7 +336,7 @@ const monacoStart = async () => {
 
     const extraTerminal = document.getElementById('control-extraTerminal');
     extraTerminal.addEventListener('click', (e) => {
-      monaco.editor.showCommands();
+      //monaco.editor.showCommands();
     });
     const extraThemeChange = document.getElementById('control-extraTheme');
     extraThemeChange.addEventListener('click', () => themeApply((Setting.getTheme) + 1));
@@ -342,6 +382,41 @@ const monacoStart = async () => {
       }
       console.log(dds, dsp);
     }
+    normalEditor.onKeyDown(function (e) {
+      if (e.code === 'Insert') {
+        isInsert = !isInsert;
+        insertIconUpdate();
+      }
+      if (isInsert === true) {
+        return null;
+      }
+      // Overwriteしたいテキストを取得する
+      if (e.browserEvent.key.length !== 1 || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) {
+        //e.preventDefault();
+        return;
+      }
+      let overwriteText = e.browserEvent.key;
+      console.log(overwriteText);
+      // カーソル位置を取得する
+      var position = normalEditor.getPosition();
+      var model = normalEditor.getModel();
+
+      // テキストを更新する
+      var editOperation = {
+        range: new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column + 1),
+        text: overwriteText
+      };
+      model.pushEditOperations([], [editOperation], null);
+
+      // カーソル位置を更新する
+      normalEditor.setPosition({
+        lineNumber: position.lineNumber,
+        column: position.column + 1
+      });
+
+      // デフォルトの動作をキャンセルする
+      e.preventDefault();
+    });
   });
 }
 window.onload = async () => {
